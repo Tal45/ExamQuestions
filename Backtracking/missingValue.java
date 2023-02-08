fa640011a336ae534c347a657030bfcbf6e3c74b
Tal Levi <117028979+Tal45@users.noreply.github.com>
2023-02-08 19:26:05 +0200

    Update missingValue.java

--- Backtracking/missingValue.java
+++ Backtracking/missingValue.java
@@ -1,16 +1,24 @@
 // 2019a 85
+// Time complexity O(logn)
+// Arithmetic progression - an = a1 + (n-1)*diff
 public class missingValue {
     public static int missingValue(int[] arr) {
         int diff = Math.min(arr[1]-arr[0], arr[2]-arr[1]);
-        int i = 0;
-        for (i = 0; i < arr.length-1; i++) {
-            if (arr[i+1] - arr[i] == diff)
-                 continue;
-            else
-                break;
-        }
+        int low = 0;
+        int mid;
+        int high = arr.length-1;
+        int missing = 0;
         
-        return arr[i]+diff;
+        while (low <= high) {
+            mid = (low+high) / 2;
+            if (arr[mid] == arr[0]+(mid*diff))
+                low = mid+1;
+            else {
+                missing = mid;
+                high = mid-1;
+            }
+        }
+        return arr[0]+(missing*diff);
     }
     public static void main(String [] args) {
         int[] arr = {7, 10, 13, 16, 22, 25};
