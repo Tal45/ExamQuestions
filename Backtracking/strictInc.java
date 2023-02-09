fc5c1f868282f5b33c8bbd1f8a07450c9685e02e
Tal Levi <117028979+Tal45@users.noreply.github.com>
2023-02-09 21:40:24 +0200

    Add files via upload

---
+++ Backtracking/strictInc.java
@@ -0,0 +1,30 @@
+// 2023a 85
+public class strictInc {
+    public static boolean strictInc (int[] arr, int k) {
+        int i = arr.length-1;
+        int sum = 0;
+        
+        for (int j = 0; j< arr.length; j++) {
+            sum += arr[j];
+        }
+        
+        while (i >= 0) {
+            if (sum == k)
+                return true;
+            else if (sum > k){
+                if (sum - arr[i] >= k) {
+                    sum-=arr[i];
+                    i--;
+                }
+                else 
+                    i--;
+            }
+        }
+        return false;
+    }
+    public static void main (String[] args) {
+        int[] arr = {1,2,3,8,27};
+        System.out.println("For k = 30: "+strictInc(arr, 30));
+        System.out.println("For k = 7 "+strictInc(arr, 7));
+    }
+}
